970a0809f50f27e0a26649924f71d9b245094c96
codemode365 <codemode365@gmail.com>
2022-04-25 06:52:47 -0700

    Message box added

--- main.js
+++ main.js
@@ -1,39 +1,70 @@
-let Onlights =document.querySelectorAll('.onlight');
+let Onlights = document.querySelectorAll('.onlight');
 let OnlightsArray = Array.from(Onlights);
 let timeOut = document.getElementById('timer');
+let message = document.getElementById('message')
 
 
 //One of the light is turned on when reloaded
-let randNum= Math.round(Math.random()*2);
-OnlightsArray[randNum].style.visibility ='visible';
+// var randNum = Math.round(Math.random() * 2);
+// OnlightsArray[randNum].style.visibility = 'visible';
 
 
 //light changer function
-let randomLight=()=>{
-    let randNum= Math.round(Math.random()*2);
-    console.log(randNum)
-    OnlightsArray.forEach((el,indx, arr)=>{
-        // console.log(el)
-        // console.log(OnlightsArray[randNum])
-        if(el!=OnlightsArray[randNum]){
-            el.style.visibility ='hidden';
-        }else{
-            el.style.visibility='visible';
+var randNew = 0;
+var randNum = 0;
+var randStore = 0;
+    let randomLight = () => {
+        randStore = randNew;
+        randNum = Math.round(Math.random() * 2);
+        randNew = randNum;
+        console.log(randNew)
+        console.log(randStore)
+
+        //to reduce repetation : only available for single repetation
+        if (randNew == randStore) {
+            console.log('donto repeat')
+            randNum = Math.round(Math.random() * 2);
+           
         }
-    })
-}
-//setInterval(randomLight, 30000)// The light changes in 1min
-// randomLight();
+
+
+        OnlightsArray.forEach((el, indx, arr) => {
+           
+            if (el != OnlightsArray[randNum]) {
+                el.style.visibility = 'hidden';
+            } else {
+                el.style.visibility = 'visible';
+            }
+        })
+        if(randNum==0){
+            message.innerText='Stop';
+            message.style.color='red';
+        }
+        else if(randNum==1){
+            message.innerText='Go';
+            message.style.color='Green';
+
+        }
+        else if(randNum==2){
+            message.innerText='Wait';
+            message.style.color='Yellow';
+
+        }
+    }
+
+//for timing part
+
 var secRemain = timeOut.getAttribute('data');
 
-let Timer = ()=>{
-    timeOut.innerText = secRemain-1; 
+let Timer = () => {
+    timeOut.innerText = secRemain - 1;
     secRemain = secRemain - 1;
     console.log(secRemain)
-    if(secRemain < 1){
+    if (secRemain < 1) {
         randomLight();
         secRemain = 30;
     }
-    
+
 }
 setInterval(Timer, 1000);
+
